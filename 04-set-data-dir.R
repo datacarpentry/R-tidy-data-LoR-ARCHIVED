b9f16bef1afb0b6f8538a9dc501a2f53bf5f78ca
Jens Preußner <jens.preussner@mpi-bn.mpg.de>
2015-09-16 09:09:00 +0200

    Prepared for pull request to datacarpentry

---
+++ 04-set-data-dir.R
@@ -0,0 +1,5 @@
+## stores the path to the data used in this lesson
+## Data Carpentry does NOT want data down in the lessons
+
+data_dir <- file.path("04-runway-usage_files")
+data_dir <- normalizePath(data_dir)
